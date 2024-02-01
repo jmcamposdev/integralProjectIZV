1e035d18904b09407a1f2e5432e01a3d85f5c133
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-01 11:25:09 +0100

    Remove the models in index.js and change the sync database method

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,13 @@
 import app from './app.js'
 import { sequelize } from './database/database.js'
 
-// Tables Models
-import './models/Formations.js'
-import './models/Groups.js'
-
 /**
  * Main function to start the server
  * @returns {Promise<void>}
  */
 async function main () {
   try {
-    await sequelize.sync({ alter: true })
+    await sequelize.sync({ force: false })
     // Start the server listening on port 3001
     app.listen(3001, () => {
       // Know that the server is running
