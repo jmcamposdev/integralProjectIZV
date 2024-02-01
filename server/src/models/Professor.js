3c56b948ebb5c98649f854c095b91441406ff16f
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-01 14:38:02 +0100

    Added Professor Model

---
+++ server/src/models/Professor.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database/database.js'
+
+/**
+ * Tabla profesor
+ * Contiene los datos de cada profesor.
+ * La tabla profesor que cuenta con los siguientes campos:
+ * id, usuario de séneca, nombre, apellido1, apellido2, especialidad.
+ * • usuario de séneca : combinación de 7 letras seguidas de 3 números sin espacio ni
+ * guiones. Ejemplo: dfuebre452 pfrolop854
+ * • especialidad: este campo sólo puede tener dos valores: "secundaria" o
+ * "formación profesional"
+ */
+export const Professor = sequelize.define('professors', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  senecaUser: {
+    type: DataTypes.STRING
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  firstSurname: {
+    type: DataTypes.STRING
+  },
+  lastSurname: {
+    type: DataTypes.STRING
+  },
+  specialty: {
+    type: DataTypes.STRING
+  }
+})
