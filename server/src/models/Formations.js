7be4a203c157047b2355abcc9e6c7a9c185d9d38
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-01 10:36:50 +0100

    Agregada tabla de formaciones en el modelo Formations.js

---
+++ server/src/models/Formations.js
@@ -0,0 +1,29 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database/database.js'
+
+/**
+ * Tabla formación
+ * Se refiere al ciclo formativo, curso de especialización, ESO, Bachillerato, etc.
+ * La tabla formación que tiene los siguientes campos:
+ * id, denominación, siglas.
+ * Ejemplos:
+ * • id: 1234
+ * • denominación: Desarrollo de Aplicaciones Web
+ * • siglas: DAW
+ * • id: 5678
+ * • denominación: Curso Especialización de Ciberseguridad
+ * • siglas: CIBER
+ */
+export const Formations = sequelize.define('formations', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  denomination: {
+    type: DataTypes.STRING
+  },
+  acronym: {
+    type: DataTypes.STRING
+  }
+})
