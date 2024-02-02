441da921fb9a490c9a0db3bd2ece122f628ee3ae
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-02 15:00:40 +0100

    Add Role model and define 1:N relation with User

---
+++ server/src/models/Role.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database/database.js'
+import { User } from './User.js'
+
+export const Role = sequelize.define('role', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  }
+})
+
+// Relation 1:N between Role and User
+Role.hasMany(User, { foreignKey: 'roleId', sourceKey: 'id' })
+User.belongsTo(Role, { foreignKey: 'roleId', targetId: 'id' })
