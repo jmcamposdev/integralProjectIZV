4beb7dd674e8fbf5e5bd14c7800406aace00f596
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:13:33 +0100

    Adding relevant comments to Groups Routes

--- server/src/routes/groups.routes.js
+++ server/src/routes/groups.routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express'
 import { createGroup, deleteGroup, getGroup, getGroups, updateGroup } from '../controllers/groups.controllers.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to do CRUD operations on groups
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle groups routes
 const router = Router()
 
-router.get('/groups', getGroups)
-router.post('/groups', createGroup)
-router.put('/groups/:id', updateGroup)
-router.delete('/groups/:id', deleteGroup)
-router.get('/groups/:id', getGroup)
+router.get('/groups', getGroups) // Get all groups
+router.post('/groups', createGroup) // Create a new group
+router.put('/groups/:id', updateGroup) // Update a group
+router.delete('/groups/:id', deleteGroup) // Delete a group
+router.get('/groups/:id', getGroup) // Get a group by id
 
+// Export router to use it in the app
 export default router
