2f6b2cd3bfa3bfbffededa82d534a9c77f989d70
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:18:30 +0100

    Add role types and define Role model

--- server/src/models/Role.js
+++ server/src/models/Role.js
@@ -2,6 +2,10 @@ import { DataTypes } from 'sequelize'
 import { sequelize } from '../database/database.js'
 import { User } from './User.js'
 
+// Define all Roles types
+export const ROLES = ['user', 'admin', 'moderator']
+
+// Define the Role model
 export const Role = sequelize.define('role', {
   id: {
     type: DataTypes.INTEGER,
