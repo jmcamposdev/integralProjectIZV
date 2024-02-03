6aa041311cda40d93a61d53227871012a77e052f
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:15:34 +0100

    Added relevant comments to Modules routes

--- server/src/routes/modules.routes.js
+++ server/src/routes/modules.routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express'
 import { createModule, deleteModule, getModule, getModules, updateModule } from '../controllers/modules.controller.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to do CRUD operations on modules
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle modules routes
 const router = Router()
 
-router.get('/modules', getModules)
-router.post('/modules', createModule)
-router.put('/modules/:id', updateModule)
-router.delete('/modules/:id', deleteModule)
-router.get('/modules/:id', getModule)
+router.get('/modules', getModules) // Get all modules
+router.post('/modules', createModule) // Create a new module
+router.put('/modules/:id', updateModule) // Update a module
+router.delete('/modules/:id', deleteModule) // Delete a module
+router.get('/modules/:id', getModule) // Get a module by id
 
+// Export router to use it in the app
 export default router
