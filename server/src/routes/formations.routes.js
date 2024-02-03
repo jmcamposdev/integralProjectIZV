0b56593fa26bfb79728ad00a64a47410b86b153b
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:12:18 +0100

    Adding relevant comments to Formations Routes

--- server/src/routes/formations.routes.js
+++ server/src/routes/formations.routes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express'
 import { getFormations, createFormation, updateFormation, deleteFormation, getFormation, getFormationGroups, getFormationsModules } from '../controllers/formations.controller.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to do CRUD operations on formations
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle formations routes
 const router = Router()
 
-router.get('/formations', getFormations)
-router.post('/formations', createFormation)
-router.put('/formations/:id', updateFormation)
-router.delete('/formations/:id', deleteFormation)
-router.get('/formations/:id', getFormation)
+router.get('/formations', getFormations) // Get all formations
+router.post('/formations', createFormation) // Create a new formation
+router.put('/formations/:id', updateFormation) // Update a formation
+router.delete('/formations/:id', deleteFormation) // Delete a formation
+router.get('/formations/:id', getFormation) // Get a formation by id
 
 // Relation between formations and groups routes
-router.get('/formations/:id/groups', getFormationGroups)
-router.get('/formations/:id/modules', getFormationsModules)
+router.get('/formations/:id/groups', getFormationGroups) // Get all groups of a formation
+router.get('/formations/:id/modules', getFormationsModules) // Get all modules of a formation
 
+// Export router to use it in the app
 export default router
