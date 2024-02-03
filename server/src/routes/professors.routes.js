2e07ec75d5536f8aee5e4a6d510dfd19066d6bec
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:16:28 +0100

    Added relevant comments to Professors Routes

--- server/src/routes/professors.routes.js
+++ server/src/routes/professors.routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express'
 import { createProfessor, deleteProfessor, getProfessor, getProfessors, updateProfessor } from '../controllers/professors.controller.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to do CRUD operations on professors
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle professors routes
 const router = Router()
 
-router.get('/professors', getProfessors)
-router.post('/professors', createProfessor)
-router.put('/professors/:id', updateProfessor)
-router.delete('/professors/:id', deleteProfessor)
-router.get('/professors/:id', getProfessor)
+router.get('/professors', getProfessors) // Get all professors
+router.post('/professors', createProfessor) // Create a new professor
+router.put('/professors/:id', updateProfessor) // Update a professor
+router.delete('/professors/:id', deleteProfessor) // Delete a professor
+router.get('/professors/:id', getProfessor) // Get a professor by id
 
+// Export router to use it in the app
 export default router
