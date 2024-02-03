70e5706f3290ab869d87e407c46aa0c22de70802
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:21:05 +0100

    Added relevant comments to Auth Routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
 import { signIn, signUp } from '../controllers/auth.controller.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to Sign Up and Sign In users
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle auth routes
 const router = Router()
 
-router.post('/auth/signup', signUp)
-router.post('/auth/signin', signIn)
+router.post('/auth/signup', signUp) // Sign Up
+router.post('/auth/signin', signIn) // Sign In
 
+// Export router to use it in the app
 export default router
