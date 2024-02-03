5817e8ccfa4db6be914643ce8cb8bf055c9d5232
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-03 12:14:30 +0100

    Added relevant comments to Lessons Routes

--- server/src/routes/lessons.routes.js
+++ server/src/routes/lessons.routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express'
 import { createLesson, deleteLesson, getLesson, getLessons, updateLesson } from '../controllers/lessons.controller.js'
 
+/**
+ * ----------------------------------------------
+ * Routes to do CRUD operations on lessons
+ * @type {Router}
+ * ----------------------------------------------
+ */
+
+// Create a new router to handle lessons routes
 const router = Router()
 
-router.get('/lessons', getLessons)
-router.post('/lessons', createLesson)
-router.put('/lessons/:id', updateLesson)
-router.delete('/lessons/:id', deleteLesson)
-router.get('/lessons/:id', getLesson)
+router.get('/lessons', getLessons) // Get all lessons
+router.post('/lessons', createLesson) // Create a new lesson
+router.put('/lessons/:id', updateLesson) // Update a lesson
+router.delete('/lessons/:id', deleteLesson) // Delete a lesson
+router.get('/lessons/:id', getLesson) // Get a lesson by id
 
+// Export router to use it in the app
 export default router
