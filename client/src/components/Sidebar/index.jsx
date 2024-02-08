d181c6c488ee0cc5c7bbb659a3d025b905c90966
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-08 18:34:49 +0100

    Add Groups menu item to Sidebar

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -181,6 +181,21 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
               </li>
               {/* <!-- Menu Item Module --> */}
 
+              {/* <!-- Menu Item Module --> */}
+              <li>
+                <NavLink
+                  to='/dashboard/groups'
+                  className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                    pathname.includes('groups') &&
+                    'bg-graydark dark:bg-meta-4'
+                  }`}
+                >
+                  <i class='icon-[tabler--users-group]' style={{ fontSize: '18px' }} />
+                  Groups
+                </NavLink>
+              </li>
+              {/* <!-- Menu Item Module --> */}
+
               {/* <!-- Menu Item Calendar --> */}
               <li>
                 <NavLink
