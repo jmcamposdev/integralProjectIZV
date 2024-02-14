c9e5dfbdeda0472ba5adef93fb7652c1afa41fa8
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-14 08:21:05 +0100

    Remove initialSetup.createRoles() and add initialSetup() function call in the index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import app from './app.js'
 import { sequelize } from './database/database.js'
+import { initialSetup } from './libs/initialSetup.js'
 
 /**
  * Main function to start the server
@@ -8,6 +9,8 @@ import { sequelize } from './database/database.js'
 async function main () {
   try {
     await sequelize.sync({ alter: true }) // If force is true, all tables are dropped and recreated
+    // Create the roles
+    await initialSetup()
     // Start the server listening on port 3001
     app.listen(3001, () => {
       // Know that the server is running
