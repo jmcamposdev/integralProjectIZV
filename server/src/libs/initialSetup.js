13a20ecc73ae27e04e8295171099c0ecc920b2d8
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-14 08:20:51 +0100

    Add initial setup functions to create roles and admin user

--- server/src/libs/initialSetup.js
+++ server/src/libs/initialSetup.js
@@ -1,5 +1,17 @@
 import { Role } from '../models/Role.js'
+import { User } from '../models/User.js'
 
+export const initialSetup = async () => {
+  await createRoles()
+  await createAdminUser()
+}
+
+/**
+ * Create then necessary roles in the database
+ * ROLES: User, Admin
+ * - If there are roles in the database, do nothing
+ * - If there are no roles in the database, create them
+ */
 export const createRoles = async () => {
   // Check if there are roles in the database
   const currentRoles = await Role.findAll()
@@ -17,4 +29,25 @@ export const createRoles = async () => {
   await Role.bulkCreate(roles)
 
   console.log('Roles created')
+}
+
+export const createAdminUser = async () => {
+  // Check if there are users in the database
+  const currentUsers = await User.findAll()
+
+  // If there are users, do nothing
+  if (currentUsers.length > 0) return
+
+  // If there are no users, create the admin user
+  const adminUser = {
+    name: 'admin',
+    email: 'admin@gmail.com',
+    password: await User.encryptPassword('admin'),
+    roleId: 2
+  }
+
+  // Create the admin user
+  await User.create(adminUser)
+
+  console.log('Admin user created')
 }
