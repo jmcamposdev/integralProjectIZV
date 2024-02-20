88cffe10f97adeeaa3ac1459722cd289f66f654b
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-02-20 15:36:02 +0100

    tried animating the nav depending on states, scrolled, scrolled on mobile resolution, the nav is opened/ close on phone resolution, ...

--- client/src/components/HeaderMain/HeaderMain.jsx
+++ client/src/components/HeaderMain/HeaderMain.jsx
@@ -8,6 +8,14 @@ import useSignOut from 'react-auth-kit/hooks/useSignOut'
 import useAuth from '../../hooks/useAuth'
 
 const Header = () => {
+  const { isLogged } = useAuth()
+  const signOut = useSignOut()
+
+  const handleSignOut = () => {
+    signOut()
+    window.location.reload()
+  }
+
   const [isScrolled, setIsScrolled] = useState(false)
 
   useEffect(() => {
@@ -19,17 +27,33 @@ const Header = () => {
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
-  const { isLogged } = useAuth()
-  const signOut = useSignOut()
 
-  const handleSignOut = () => {
-    signOut()
-    window.location.reload()
+  const nonScrolledNavStyle = () => {
+    const nav = document.getElementById('nav')
+
+    nav.classList.remove('py-8')
+    nav.classList.remove('bg-transparent')
+
+    nav.classList.add('py-4')
+    nav.classList.add('bg-white')
+    nav.classList.add('dark:bg-boxdark')
+  }
+
+  const scrolledNavStyle = () => {
+    const nav = document.getElementById('nav')
+
+    nav.classList.remove('py-4')
+    nav.classList.remove('bg-white')
+    nav.classList.remove('dark:bg-boxdark')
+
+    nav.classList.add('py-8')
+    nav.classList.add('bg-transparent')
   }
 
   const openHideNav = () => {
-    const nav = document.getElementById('mobile-menu-2')
+    const mobileMenu = document.getElementById('mobile-menu-2')
     const ul = document.getElementById('ul')
+
     if (ul.classList.contains('flex-row')) {
       ul.classList.remove('flex-row')
       ul.classList.add('flex-col')
@@ -37,15 +61,39 @@ const Header = () => {
       ul.classList.add('flex-row')
       ul.classList.remove('flex-col')
     }
-    if (nav.classList.contains('hidden')) {
-      nav.style.minHeight = '0px'
-    }
-    nav.classList.toggle('hidden')
+
+    mobileMenu.classList.toggle('hidden')
   }
+
+  const headerAnimation = () => {
+    // ${ isScrolled ? 'py-4 bg-white dark:bg-boxdark' : 'bg-transparent py-8' }
+    const mobileMenu = document.getElementById('mobile-menu-2')
+
+    if (!mobileMenu) {
+      return
+    }
+
+    if (isScrolled && window.screen.width > 1024) {
+      scrolledNavStyle()
+    } else if (!isScrolled && window.screen.width > 1024) {
+      nonScrolledNavStyle()
+    } else if (isScrolled && window.screen.width <= 1024 && mobileMenu.classList.contains('hidden')) {
+      scrolledNavStyle()
+    } else if (isScrolled && window.screen.width <= 1024 && !mobileMenu.classList.contains('hidden')) {
+      scrolledNavStyle()
+    } else if (!isScrolled && window.screen.width <= 1024 && mobileMenu.classList.contains('hidden')) {
+      nonScrolledNavStyle()
+    } else if (!isScrolled && window.screen.width <= 1024 && !mobileMenu.classList.contains('hidden')) {
+      scrolledNavStyle()
+    }
+  }
+
+  document.body.addEventListener('scroll', headerAnimation)
+  document.body.addEventListener('DOMContentLoaded', headerAnimation)
 
   return (
     <header className='fixed z-99 w-[100dvw]'>
-      <nav className={`bg-white border-gray-200 px-4 lg:px-6 dark:bg-gray-800 transition-all ${isScrolled ? 'py-4 bg-white dark:bg-boxdark' : 'bg-transparent py-8'}`}>
+      <nav id='nav' className='bg-white border-gray-200 px-4 lg:px-6 transition-all'>
         <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
           <a href='https://flowbite.com' className='flex items-center'>
             <img src={LogoIcon} className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
@@ -91,7 +139,7 @@ const Header = () => {
             <ul className='items-center gap-2 2xsm:gap-4 hidden lg:flex'>
               <DarkModeSwitcher />
             </ul>
-            <button onClick={openHideNav} id='button-nav' data-collapse-toggle='mobile-menu-2' type='button' className='inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
+            <button onClick={() => { openHideNav(); headerAnimation() }} id='button-nav' data-collapse-toggle='mobile-menu-2' type='button' className='inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
               <span className='sr-only'>Open main menu</span>
               <svg className='w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clipRule='evenodd' /></svg>
               <svg className='hidden w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clipRule='evenodd' /></svg>
