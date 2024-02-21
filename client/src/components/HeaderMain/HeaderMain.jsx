656fb8a95b198b52cc063883321676fe2de0db48
Carlos Hernández <cherpal2010@ieszaidinvergeles.org>
2024-02-21 19:40:34 +0100

    Finished the nav

--- client/src/components/HeaderMain/HeaderMain.jsx
+++ client/src/components/HeaderMain/HeaderMain.jsx
@@ -28,75 +28,75 @@ const Header = () => {
     return () => window.removeEventListener('scroll', handleScroll)
   }, [])
 
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 1024);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  const nonScrolledNavStyle = () => {
-    const nav = document.getElementById('nav')
-
-    nav.classList.remove('py-8')
-    nav.classList.remove('bg-transparent')
-
-    nav.classList.add('py-4')
-    nav.classList.add('bg-white')
-    nav.classList.add('dark:bg-boxdark')
-  }
-
-  const scrolledNavStyle = () => {
-    const nav = document.getElementById('nav')
-
-    nav.classList.remove('py-4')
-    nav.classList.remove('bg-white')
-    nav.classList.remove('dark:bg-boxdark')
-
-    nav.classList.add('py-8')
-    nav.classList.add('bg-transparent')
-  }
-
   const openHideNav = () => {
-    const mobileMenu = document.getElementById('mobile-menu-2')
-    const ul = document.getElementById('ul')
+    const backNavPhone = document.getElementById('back-nav-phone')
+    const buttonNav = document.getElementById('button-nav')
 
+    buttonNav.children.item(0).classList.toggle('rotate-45')
+    buttonNav.children.item(0).classList.toggle('top-[15%]')
+    buttonNav.children.item(0).classList.toggle('top-[50%]')
+    buttonNav.children.item(1).classList.toggle('opacity-0')
+    buttonNav.children.item(2).classList.toggle('-rotate-45')
+    buttonNav.children.item(2).classList.toggle('top-[15%]')
+    buttonNav.children.item(2).classList.toggle('top-[50%]')
 
+    buttonNav.classList.toggle('justify-center')
+    buttonNav.classList.toggle('justify-between')
+    backNavPhone.classList.toggle('h-0')
+    backNavPhone.classList.toggle('h-[100dvh]')
 
-    if (ul.classList.contains('flex-row')) {
-      ul.classList.remove('flex-row')
-      ul.classList.add('flex-col')
+    if (document.body.style.overflow === 'hidden') {
+      document.body.style.overflow = 'visible'
     } else {
-      ul.classList.add('flex-row')
-      ul.classList.remove('flex-col')
-    }
-
-    mobileMenu.classList.toggle('hidden')
-
-    if (!mobileMenu.classList.contains('hidden')) {
-      nonScrolledNavStyle()
-    } else {
-      scrolledNavStyle()
+      document.body.style.overflow = 'hidden'
     }
   }
-
-
-
-  const navClass = isScrolled && isDesktop
-    ? 'py-4 bg-white dark:bg-boxdark'
-    : !isScrolled && isDesktop
-      ? 'py-8 bg-transparent'
-      : '';
-
 
   return (
     <header className='fixed z-99 w-[100dvw]'>
 
-      <nav id='nav' className={`${navClass} border-gray-200 px-4 lg:px-6 transition-all`}>
+      <nav id='nav' className={`${isScrolled ? 'py-4 bg-white dark:bg-boxdark' : 'py-8 bg-transparent'} border-gray-200 px-4 lg:px-6 transition-all relative`}>
+        <div id='back-nav-phone' className='absolute w-[100%] h-0  left-0 top-0 -z-1 bg-white dark:bg-boxdark transition-all overflow-hidden duration-1000'>
+          <div className={`${isScrolled ? 'mt-[80px]' : 'mt-[112px]'} px-4 flex flex-col gap-[1rem]`}>
+
+            <ul id='ul' className='flex flex-col mt-4 font-medium  lg:space-x-8 lg:mt-0'>
+              <li>
+                <a href='/' className='block py-8 pr-8 pl-3  lg:p-0  text-3xl hover:text-blue-600 dark:hover:text-blue-600 sm:py-6  text-dark dark:text-white' aria-current='page'>Home</a>
+              </li>
+              <li>
+                <a href='/' className='block py-8 pr-8 pl-3  lg:p-0  text-3xl hover:text-blue-600 dark:hover:text-blue-600 sm:py-6  text-dark dark:text-white' aria-current='page'>About</a>
+              </li>
+              <li>
+                <a href='/' className='block py-8 pr-8 pl-3  lg:p-0  text-3xl hover:text-blue-600 dark:hover:text-blue-600 sm:py-6  text-dark dark:text-white' aria-current='page'>Team</a>
+              </li>
+              <li>
+                <a href='/' className='block py-8 pr-8 pl-3  lg:p-0  text-3xl hover:text-blue-600 dark:hover:text-blue-600 sm:py-6  text-dark dark:text-white' aria-current='page'>Contact</a>
+              </li>
+              {isLogged
+                ? (
+                  <li>
+                    <a
+                      onClick={handleSignOut}
+                      className='w-fit text-3xl block py-3 px-7 font-bold text-dark hover:opacity-70 dark:text-white md:block'
+                    >
+                      Sign Out
+                    </a>
+                  </li>
+                  )
+                : (
+                  <li className='py-6'>
+                    <a
+                      href='/login'
+                      className='w-fit text-3xl block ease-in-up rounded-md bg-primary py-3 px-8 font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9'
+                    >
+                      Sign In
+                    </a>
+                  </li>
+                  )}
+
+            </ul>
+          </div>
+        </div>
         <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
           <a href='/' className='flex items-center'>
             <img src={LogoIcon} className='mr-3 h-6 sm:h-9' alt='Flowbite Logo' />
@@ -106,50 +106,30 @@ const Header = () => {
               ? (
                 <a
                   onClick={handleSignOut}
-                  className='py-3 px-7 text-base font-bold text-dark hover:opacity-70 dark:text-white md:block'
+                  className=' hidden lg:block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
                 >
                   Sign Out
                 </a>
-              )
+                )
               : (
                 <a
                   href='/login'
-                  className='py-3 px-7 text-base font-bold text-dark hover:opacity-70 dark:text-white md:block'
+                  className=' hidden lg:block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
                 >
                   Sign In
                 </a>
-              )}
-            {isLogged
-              ? (
-                <a
-                  href='/dashboard'
-                  className='ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9'
-                >
-                  Dashboard
-                </a>
-              )
-              : null}
-            {!isLogged
-              ? (
-                <a
-                  href='/signup'
-                  className='ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp md:block md:px-9 lg:px-6 xl:px-9'
-                >
-                  Sign Up
-                </a>
-              )
-              : null}
-            <ul className='items-center gap-2 2xsm:gap-4 hidden lg:flex'>
+                )}
+            <ul className='items-center gap-2 2xsm:gap-4 flex'>
               <DarkModeSwitcher />
             </ul>
-            <button onClick={openHideNav} id='button-nav' data-collapse-toggle='mobile-menu-2' type='button' className='inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
-              <span className='sr-only'>Open main menu</span>
-              <svg className='w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clipRule='evenodd' /></svg>
-              <svg className='hidden w-6 h-6' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fillRule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clipRule='evenodd' /></svg>
+            <button onClick={openHideNav} id='button-nav' data-collapse-toggle='mobile-menu-2' type='button' className='relative w-[40px] h-[36px] inline-flex justify-center flex-col p-1.5 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600' aria-controls='mobile-menu-2' aria-expanded='false'>
+              <div className='absolute top-[15%] transition-all block origin-center w-[30px] h-[2px] dark:bg-white bg-stone-400' />
+              <div className='transition-all w-[24px] h-[2px] dark:bg-white bg-stone-400 ml-auto' />
+              <div className='absolute bottom-[15%] transition-all block origin-center w-[30px] h-[2px] dark:bg-white bg-stone-400' />
             </button>
           </div>
           <div className='hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1' id='mobile-menu-2'>
-            <ul id='ul' className='flex flex-row mt-4 font-medium  lg:space-x-8 lg:mt-0'>
+            <ul id='ul' className='hidden lg:flex flex-row mt-4 font-medium  lg:space-x-8 lg:mt-0'>
               <li>
                 <a href='/' className={`block py-2 pr-4 pl-3  lg:p-0  font-medium  hover:text-blue-600 dark:hover:text-blue-600 sm:py-6  ${isScrolled ? 'text-gray-500 dark:text-white' : 'text-dark'}`} aria-current='page'>Home</a>
               </li>
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -5,35 +5,31 @@ const Hero = () => {
   const { isLogged } = useAuth()
 
   return (
-    <section id='home' className='static bg-cover min-h-screen relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px] dark:bg-boxdark dark:drop-shadow-none'>
+    <section id='home' className='flex align-center static bg-cover min-h-screen relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px] dark:bg-boxdark dark:drop-shadow-none'>
       <div className='absolute w-[100%] h-[100%] top-0 left-0 -z-1 brightness-75' style={{ backgroundImage: `url(${HeroImg})` }} />
       <div className='flex justify-start px-[7%]'>
         <div className='-mx-4 flex flex-wrap'>
           <div className='w-full px-4'>
             <div className='wow fadeInUp mx-auto max-w-[800px] ' data-wow-delay='.2s'>
               <h1 className='mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight'>Free and Open-Source Next.js Template for Startup &amp; SaaS</h1>
-              <p className='mb-12 text-base font-medium !leading-relaxed text-black dark:text-white dark:opacity-90 sm:text-lg md:text-xl'>Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.</p>
               <div className='flex flex-col items-center justify-start space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0'>
                 {isLogged
                   ? (
                     <a
-                      href='/dashboard'
-                      className='rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80'
+                      onClick={handleSignOut}
+                      className=' hidden lg:block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
                     >
-                      Dashboard
+                      Sign Out
                     </a>
                     )
-                  : null}
-                {!isLogged
-                  ? (
+                  : (
                     <a
-                      href='/signup'
-                      className='rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80'
+                      href='/login'
+                      className=' hidden lg:block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
                     >
-                      Sign Up
+                      Sign In
                     </a>
-                    )
-                  : null}
+                    )}
               </div>
             </div>
           </div>
