75b7b4128e76470e1884fc989f4a3b52fcb01b71
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-23 15:12:10 +0100

    Remove unnecessary senecaUser field from user object

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -5,6 +5,11 @@ const userService = {
   updateUser: async (user) => {
     // Get the user from the token
     const { senecaUser } = useAuth()
+
+    if (senecaUser === user.senecaUser) {
+      delete user.senecaUser
+    }
+
     try {
       // Update the user in the database
       const updatedUser = await api.put(`users/${senecaUser}`, user)
