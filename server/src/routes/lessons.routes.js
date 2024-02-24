d76f6bb73ce8eaef6c38c53a89901ced28dbfd8c
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-24 20:47:26 +0100

    Add getCurrentYearLessons endpoint to lessons controller and routes

--- server/src/routes/lessons.routes.js
+++ server/src/routes/lessons.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createLesson, deleteLesson, getLesson, getLessons, updateLesson } from '../controllers/lessons.controller.js'
+import { createLesson, deleteLesson, getCurrentYearLessons, getLesson, getLessons, updateLesson } from '../controllers/lessons.controller.js'
 import { authJwt } from '../middlewares/index.js'
 
 /**
@@ -17,6 +17,6 @@ router.post('/lessons', [authJwt.verifyToken, authJwt.isAdmin], createLesson) //
 router.put('/lessons/:id', [authJwt.verifyToken, authJwt.isAdmin], updateLesson) // Update a lesson
 router.delete('/lessons/:id', [authJwt.verifyToken, authJwt.isAdmin], deleteLesson) // Delete a lesson
 router.get('/lessons/:id', getLesson) // Get a lesson by id
-
+router.get('/lessons-current-year', getCurrentYearLessons) // Get all lessons, professors, modules from the current year
 // Export router to use it in the app
 export default router
