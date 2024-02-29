c903b6c0dac5fdeb55be1800d628966232927f29
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-29 07:02:22 +0100

    Refactor Hero component to remove unnecessary code and update Classes link

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -20,23 +20,12 @@ const Hero = () => {
             <div className='wow fadeInUp mx-auto max-w-[800px] ' data-wow-delay='.2s'>
               <h1 className='mb-5 text-3xl font-bold leading-tight text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight'>The best app to manage your educational center successfully!</h1>
               <div className='flex flex-col items-center justify-start space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0'>
-                {isLogged
-                  ? (
-                    <a
-                      onClick={handleSignOut}
-                      className='block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
-                    >
-                      Sign Out
-                    </a>
-                    )
-                  : (
-                    <a
-                      href='/login'
-                      className='block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9'
-                    >
-                      Sign In
-                    </a>
-                    )}
+                <a
+                  href='#classes'
+                  className='block ease-in-up rounded-md bg-primary py-3 px-8 text-base font-bold text-white transition duration-300 hover:bg-opacity-90 hover:shadow-signUp  md:px-9 lg:px-6 xl:px-9 cursor-pointer'
+                >
+                  Classes
+                </a>
               </div>
             </div>
           </div>
