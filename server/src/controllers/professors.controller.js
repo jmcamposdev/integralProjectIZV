516bf7e4b6e5008125fd0112f19f5867f6bd849c
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-29 07:58:06 +0100

    Remove validation for senecaUser field in updateProfessor function

--- server/src/controllers/professors.controller.js
+++ server/src/controllers/professors.controller.js
@@ -140,11 +140,6 @@ export const updateProfessor = async (req, res) => {
       return res.status(400).json({ message: 'All fields are required and not empty' })
     }
 
-    // Validate that the seneceUser is not empty
-    if (!req.body.senecaUser) {
-      return res.status(400).json({ message: 'The senecaUser field is required' })
-    }
-
     // If is defined the senecaUser, validate that there's no other professor with the same senecaUser or user
     if (req.body.senecaUser && (await professorExists(req.body.senecaUser) || await User.exists(req.body.senecaUser))) {
       return res.status(400).json({ message: 'Already exists a professor with that senecaUser' })
