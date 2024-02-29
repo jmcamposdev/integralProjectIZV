f15105dec0364ebde6dcf807fbb098501af01bda
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-02-29 07:09:12 +0100

    Update password field label and placeholder

--- client/src/pages/Authentication/SignIn.jsx
+++ client/src/pages/Authentication/SignIn.jsx
@@ -254,7 +254,7 @@ const SignIn = () => {
 
                 <div className='mb-6'>
                   <label className='mb-2.5 block font-medium text-black dark:text-white'>
-                    Re-type Password
+                    Password
                   </label>
                   <div className='relative'>
                     <input
@@ -262,7 +262,7 @@ const SignIn = () => {
                       name='password'
                       value={inputs.password}
                       onChange={handledInputChange}
-                      placeholder='6+ Characters, 1 Capital letter'
+                      placeholder='Password'
                       className='w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary'
                     />
 
