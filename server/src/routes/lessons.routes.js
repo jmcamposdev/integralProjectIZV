56a9f61cb227e745aecece7c21db39470db5a4c7
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-02 21:06:36 +0100

    Add lessons-generate endpoint to lessons.routes.js

--- server/src/routes/lessons.routes.js
+++ server/src/routes/lessons.routes.js
@@ -14,10 +14,10 @@ const router = Router()
 
 router.get('/lessons', getLessons) // Get all lessons
 router.post('/lessons', [authJwt.verifyToken, authJwt.isAdmin], createLesson) // Create a new lesson
+router.post('/lessons-generate', [authJwt.verifyToken, authJwt.isAdmin], generateLessons) // Generate lessons for all Groups and Modules and the Professor to Null
 router.put('/lessons/:id', [authJwt.verifyToken, authJwt.isAdmin], updateLesson) // Update a lesson
 router.delete('/lessons/:id', [authJwt.verifyToken, authJwt.isAdmin], deleteLesson) // Delete a lesson
 router.get('/lessons/:id', getLesson) // Get a lesson by id
-router.get('/lessons-generate', [authJwt.verifyToken, authJwt.isAdmin], generateLessons) // Generate lessons for all Groups and Modules and the Professor to Null
 router.get('/lessons-current-year', getCurrentYearLessons) // Get all lessons, professors, modules from the current year
 // Export router to use it in the app
 export default router
